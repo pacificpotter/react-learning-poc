d825b7cbf42aafc84b744e172c0384dd1a86b6a5
Prashant Gajanan Kumbhar <AL6167@accionlabs.in>
2024-10-18 15:38:21 +0530

    Calculator functionality using useState and handling event

--- Projects/calculator-v-1/src/components/ButtonsContainer.jsx
+++ Projects/calculator-v-1/src/components/ButtonsContainer.jsx
@@ -1,6 +1,6 @@
 import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = () => {
+const ButtonsContainer = ({ onButtonClick }) => {
   const buttonNames = [
     "C",
     "1",
@@ -23,7 +23,11 @@ const ButtonsContainer = () => {
   return (
     <div className={styles.buttonsContainer}>
       {buttonNames.map((buttonName) => (
-        <button key={buttonName} className={styles.button}>
+        <button
+          key={buttonName}
+          className={styles.button}
+          onClick={() => onButtonClick(buttonName)}
+        >
           {buttonName}
         </button>
       ))}
--- Projects/calculator-v-1/src/components/Display.jsx
+++ Projects/calculator-v-1/src/components/Display.jsx
@@ -1,7 +1,9 @@
 import styles from "./Display.module.css";
 
-const Display = () => {
-  return <input type="text" className={styles.display} />;
+const Display = ({ displayVal }) => {
+  return (
+    <input type="text" className={styles.display} value={displayVal} readOnly />
+  );
 };
 
 export default Display;
